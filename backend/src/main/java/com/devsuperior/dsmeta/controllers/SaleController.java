e59e6565cbccf9eb27d8789cf332320e90d62748
Armando Machado <armandostudos@gmail.com>
2022-09-10 11:13:50 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,8 +1,10 @@
-package com.devsuperior.dsmeta.controlers;
+package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,7 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public List<Sale> findSale(){
-		return service.findSales();
+	public Page<Sale> findSale(Pageable pageable){
+		return service.findSales (pageable);
 	}
 }
