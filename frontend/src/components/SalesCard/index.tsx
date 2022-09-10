bb87699007df4c87589ab729b284b14c678e14fb
Armando Machado <armandostudos@gmail.com>
2022-09-10 19:56:48 -0300

    ep3

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import NotificationButton from '../NotificationButton'
+import NotificationButton from '../NotificationButton';
 import './styles.css'
 
 function SalesCard() {
@@ -102,4 +102,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
